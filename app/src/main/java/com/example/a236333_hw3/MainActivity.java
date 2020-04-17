5a3875184f1334449eebad3104b30e8ca989f708
alonBind <alonbind@gmail.com>
2020-04-17 14:21:02 +0300

    מסך התחברות - עובד :O

--- app/src/main/java/com/example/a236333_hw3/MainActivity.java
+++ app/src/main/java/com/example/a236333_hw3/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TasksActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
     }
