157d40798f9cdd53a2ef3ad60889050bb0793043
Hila Kornis <34451280+hilakornis@users.noreply.github.com>
2020-04-17 23:02:21 +0300

    SignupActivity : Can now add Document with the user Data . todo : DocumentId is not the email of the user.

--- app/src/main/java/com/example/a236333_hw3/SignupActivity.java
+++ app/src/main/java/com/example/a236333_hw3/SignupActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.a236333_hw3.Tools.RoboCodeSettings;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,6 +22,9 @@ import android.widget.Button;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -31,15 +36,19 @@ public class SignupActivity extends AppCompatActivity {
     // ============================================================================================
 
     String TAG = "SignupActivity";
+
     FirebaseAuth firebaseAuthenticator;
     FirebaseFirestore firebaseFirestore;
 
+    String userID;
+    String userEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         firebaseAuthenticator = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
         initUI();
     }
 
@@ -85,7 +94,7 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void trySignUp() {
-        String email, password, nickname;
+        final String email, password, nickname;
         email = EmailEditText.getText().toString();
         password = passwordEditText.getText().toString();
         nickname = nicknameEditText.getText().toString();
@@ -137,6 +146,34 @@ public class SignupActivity extends AppCompatActivity {
                 // TODO : add document to USERS collection
 
                 // ------------------------------------
+                userID = firebaseAuthenticator.getCurrentUser().getUid();
+//                userEmail = firebaseAuthenticator.getCurrentUser().getEmail();
+
+
+//                DocumentReference userDocumentRef = firebaseFirestore.collection("Users").document(userEmail);
+                Map<String,Object> user = new HashMap<>();
+                user.put("Grade",0);
+                user.put("Name",nickname);
+                user.put("UID",userID);
+//                user.put("FinishedTasks",new int[]());
+//
+//                userDocumentRef.set(user);//todo not working!
+
+                // Add a new document with a generated ID
+                firebaseFirestore.collection("Users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
 //                DocumentReference
 
                 // ------------------------------------
