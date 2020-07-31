a73c362e839e6aabac05b405895122faac04b603
alonBind <alonbind@gmail.com>
2020-07-31 15:05:07 +0300

    fixing high score

--- app/src/main/java/com/example/a236333_hw3/NavigationActivity.java
+++ app/src/main/java/com/example/a236333_hw3/NavigationActivity.java
@@ -40,7 +40,13 @@ public class NavigationActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_whats_new, R.id.nav_pairing, R.id.nav_help, R.id.nav_tasks, R.id.nav_high_score, R.id.nav_settings, R.id.nav_logout)
+                R.id.nav_whats_new,
+                R.id.nav_pairing,
+               /* R.id.nav_help,*/
+                R.id.nav_tasks,
+                R.id.nav_high_score,
+               /* R.id.nav_settings, */
+                R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
--- app/src/main/java/com/example/a236333_hw3/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/a236333_hw3/ui/slideshow/SlideshowFragment.java
@@ -13,6 +13,13 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.a236333_hw3.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SlideshowFragment extends Fragment {
 
@@ -22,8 +29,59 @@ public class SlideshowFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        final TextView first = root.findViewById(R.id.first_place);
+        final TextView second = root.findViewById(R.id.second_place);
+        final TextView third = root.findViewById(R.id.third_place);
 
-        //final TextView textView = root.findViewById(R.id.text_slideshow);
+        CollectionReference tasks =
+                FirebaseFirestore.getInstance().collection("Users");
+
+        tasks.orderBy("Grade", Query.Direction.DESCENDING).limit(3).get().addOnSuccessListener(
+                new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int count = 1;
+
+                        for (final DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            final int curr = count;
+                            SlideshowFragment.this.getActivity().runOnUiThread(
+                                new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (curr == 1) first.setText("1st: " +
+                                                documentSnapshot.getString("Name") +
+                                                ", " +
+                                                documentSnapshot.getLong("Grade").toString());
+                                        if (curr == 2) second.setText("2nd: " +
+                                                documentSnapshot.getString("Name") +
+                                                ", " +
+                                                documentSnapshot.getLong("Grade").toString());
+                                        if (curr == 3) third.setText("3rd: " +
+                                                documentSnapshot.getString("Name") +
+                                                ", " +
+                                                documentSnapshot.getLong("Grade").toString());
+                                    }
+                                }
+                            );
+
+                            count ++;
+                            if (count > 3) break;
+                        }
+                        if (count <= 1) first.setText("No results yet...");
+                        if (count <= 2) second.setText("");
+                        if (count <= 3) third.setText("");
+                    }
+                }
+        ).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        first.setText("something went wrong, the connection with server is lost :(");
+                        second.setText("");
+                        third.setText("");
+                    }
+                }
+        );
 
         return root;
     }
